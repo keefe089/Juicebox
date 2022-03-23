53af8f9fb75d272b205087c9ba51793487782170
keefe089e <kelleybkeefer@googlemail.com>
2022-03-23 12:51:35 -0500

    set up

---
+++ db/index.js
@@ -0,0 +1,48 @@
+const { Client } = require("pg"); // imports the pg module
+
+// supply the db name and location of the database
+const client = new Client("postgres://localhost:5432/juicebox-dev");
+
+module.exports = {
+  client,
+};
+
+async function createUser({ username, password }) {
+  try {
+    const result = await client.query(
+      `
+        INSERT INTO users(username, password)
+        VALUES ($1, $2)
+        ON CONFLICT (username) DO NOTHING 
+      RETURNING *;
+      `,
+      [username, password]
+    );
+
+    return result;
+  } catch (error) {
+    throw error;
+  }
+  return rows;
+}
+
+// later
+module.exports = {};
+// inside db/index.js
+
+async function getAllUsers() {
+  const { rows } = await client.query(
+    `SELECT id, username 
+      FROM users;
+    `
+  );
+
+  return rows;
+}
+
+// and export them
+module.exports = {
+  client,
+  getAllUsers,
+  createUser,
+};
